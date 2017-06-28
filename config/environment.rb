466628de4a599a86d327cb14bcae7ed45ee98dae
ICanDoAllThingThroughChrist <ctlee9096@gmail.com>
2017-06-28 17:01:08 -0500

    db progress

--- config/environment.rb
+++ config/environment.rb
@@ -6,7 +6,7 @@ require 'pry-stack_explorer'
 require 'pry-doc'
 
 DB = {:conn =>SQLite3::Database.new("db/tracking.sqlite")}
-DB[:conn].executre("DROP TABLE IF EXISTS tracking")
+DB[:conn].execute("DROP TABLE IF EXISTS tracking")
 
 sql = <<-SQL
 	CREATE TABLE IF NOT EXISTS tracking (
@@ -20,34 +20,34 @@ sql = <<-SQL
 	FOREIGN KEY(site_id) REFERENCES site(id),
 	FOREIGN KEY(frequency_id) REFERENCES frequency(id),
 	FOREIGN KEY(reporting_id) REFERENCES reporting(id),
-	FOREIGN KEY(comments_id) REFERENCES comments(id)
+	FOREIGN KEY(comments_id) REFERENCES comments(id),
 	)
-#http://alvinalexander.com/android/sqlite-foreign-keys-example
+
 	CREATE TABLE IF NOT EXISTS task (
 	id INTEGER PRIMARY KEY,
-	task TEXT#INNER JOIN
+	task TEXT,#INNER JOIN
 	)
 
 	CREATE TABLE IF NOT EXISTS site (
 	id INTEGER PRIMARY KEY,
-	site TEXT#INNER JOIN
+	site TEXT,#INNER JOIN
 	)
 
 	CREATE TABLE IF NOT EXISTS frequency (
 	id INTEGER PRIMARY KEY,
-	frequency TEXT#INNER JOIN
+	frequency TEXT,#INNER JOIN
 	)
 
 	CREATE TABLE IF NOT EXISTS reporting (
 	id INTEGER PRIMARY KEY,
-	reporting TEXT#INNER JOIN
+	reporting TEXT,#INNER JOIN
 	)
 
 	CREATE TABLE IF NOT EXISTS comments (
 	id INTEGER PRIMARY KEY,
-	comments TEXT#INNER JOIN
+	comments TEXT,#INNER JOIN
 	)
-SQL
+	SQL
 	
 	DB[:conn].execute(sql)
 	DB[:conn].results_as_hash = true
